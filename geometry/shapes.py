45e713318142f3504b95492b7bb6ca8d77001816
Thomas Ha <thomasha.1310@gmail.com>
2025-07-08 14:17:41 -0400

    Add validation for inputs

--- geometry/shapes.py
+++ geometry/shapes.py
@@ -8,14 +8,20 @@ class Shape(ABC):
         
 class Square(Shape):
     def __init__(self, side: float):
+        if not isinstance(side, (int, float)):
+            raise ValueError("Side length must be numeric.")
         super().__init__()
         self.side = side
     
     def area(self) -> float:
+        if (self.side < 0):
+            raise ValueError("Side length must be non-zero to calculate area.")
         return self.side ** 2
     
 class Circle(Shape):
     def __init__(self, radius: float):
+        if not isinstance(radius, (int, float)):
+            raise ValueError("Radius length must be numeric.")
         super().__init__()
         self.radius = radius
     
